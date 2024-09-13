487723b3758aae9228632780ac71d74f80439563
sugimoto_e <eikun1014@gmail.com>
2024-09-14 00:20:35 +0900

    chore: rename file and add test

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -1,6 +1,10 @@
 package parser
 
-import "github.com/BurntSushi/toml"
+import (
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
 
 type Server struct {
 	Host string `toml:"host"`
@@ -10,7 +14,12 @@ type Server struct {
 func Parse() (map[string]Server, error) {
 	ServerMap := map[string]Server{}
 
-	_, err := toml.DecodeFile("/var/lib/ngonx/config.toml", &ServerMap)
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		configPath = "/var/lib/ngonx/config.toml"
+	}
+
+	_, err := toml.DecodeFile(configPath, &ServerMap)
 	if err != nil {
 		return nil, err
 	}
---
+++ pkg/parser/parser_test.go
@@ -0,0 +1,60 @@
+package parser_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/ei-sugimoto/ngonx/pkg/parser"
+)
+
+func TestParse(t *testing.T) {
+	// テスト用のTOMLファイルを作成
+	testConfig := `
+[server1]
+host = "localhost"
+port = 8080
+
+[server2]
+host = "example.com"
+port = 9090
+`
+	// 一時ファイルを作成
+	tmpfile, err := os.CreateTemp("", "config.toml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name()) // テスト終了後に削除
+
+	// 一時ファイルに書き込む
+	if _, err := tmpfile.Write([]byte(testConfig)); err != nil {
+		t.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	// 元のファイルパスを一時ファイルのパスに変更
+	originalPath := "/var/lib/ngonx/config.toml"
+	os.Setenv("CONFIG_PATH", tmpfile.Name())
+	defer os.Setenv("CONFIG_PATH", originalPath)
+
+	// Parse関数を呼び出す
+	serverMap, err := parser.Parse()
+	if err != nil {
+		t.Fatalf("Parse() error = %v", err)
+	}
+
+	// 結果を検証
+	expected := map[string]parser.Server{
+		"server1": {Host: "localhost", Port: 8080},
+		"server2": {Host: "example.com", Port: 9090},
+	}
+
+	for key, expectedServer := range expected {
+		if server, ok := serverMap[key]; !ok {
+			t.Errorf("expected server %s not found", key)
+		} else if server != expectedServer {
+			t.Errorf("server %s = %v, want %v", key, server, expectedServer)
+		}
+	}
+}
